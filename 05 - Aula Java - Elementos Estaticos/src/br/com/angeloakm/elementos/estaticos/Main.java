94de272b6a80e83d50fd195a126c38bceef7bc70
Angelo Ferreira <angelo.akm@gmail.com>
2016-03-01 23:06:08 -0300

    Projeto sobre elementos estaticos

---
+++ 05 - Aula Java - Elementos Estaticos/src/br/com/angeloakm/elementos/estaticos/Main.java
@@ -0,0 +1,12 @@
+package br.com.angeloakm.elementos.estaticos;
+
+public class Main {
+	public static void main(String[] args) {
+
+		int resultado = Matematica.somar(10, 5);
+		
+		System.out.println(resultado);
+
+	}
+
+}
---
+++ 05 - Aula Java - Elementos Estaticos/src/br/com/angeloakm/elementos/estaticos/Matematica.java
@@ -0,0 +1,13 @@
+package br.com.angeloakm.elementos.estaticos;
+
+public class Matematica {
+
+	public static int somar(int x, int z) {
+		return x + z;
+	}
+
+	public static int subtrair(int x, int z) {
+		return x - z;
+	}
+
+}

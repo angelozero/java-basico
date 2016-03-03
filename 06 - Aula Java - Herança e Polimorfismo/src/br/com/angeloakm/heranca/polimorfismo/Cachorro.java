49ee131da6e0769b527459e457226bfa9abdc535
Angelo Ferreira <angelo.akm@gmail.com>
2016-03-03 00:00:14 -0300

    Herança e Polimorfismo

---
+++ 06 - Aula Java - Herança e Polimorfismo/src/br/com/angeloakm/heranca/polimorfismo/Cachorro.java
@@ -0,0 +1,11 @@
+package br.com.angeloakm.heranca.polimorfismo;
+
+public class Cachorro extends Animal {
+
+	@Override
+	public void emitirSom() {
+		// TODO Auto-generated method stub
+		super.emitirSom();
+	}
+
+}
---
+++ 06 - Aula Java - Herança e Polimorfismo/src/br/com/angeloakm/heranca/polimorfismo/Main.java
@@ -0,0 +1,11 @@
+package br.com.angeloakm.heranca.polimorfismo;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Cachorro cao = new Cachorro();
+
+		cao.emitirSom();
+
+	}
+}
